2ce3e008f725d7676ba690ff16ba21cf34e53469
Aaron Kagan <aaronkagandev@gmail.com>
2022-12-15 22:11:57 -0500

    adding alt

--- src/data/imageData.js
+++ src/data/imageData.js
@@ -2,36 +2,43 @@ export const imageData = [
   {
     id: 1,
     year: 2016,
-    imageSrc: require("../assets/images/leaves.jpg")
+    imageSrc: require("../assets/images/leaves.jpg"),
+    alt: "green leaves on tree"
   },
   {
     id: 2,
     year: 2020,
-    imageSrc: require("../assets/images/bw-portrait.jpg")
+    imageSrc: require("../assets/images/bw-portrait.jpg"),
+    alt: "black and white portrait"
   },
   {
     id: 3,
     year: 2015,
-    imageSrc: require("../assets/images/man-on-phone.jpg")
+    imageSrc: require("../assets/images/man-on-phone.jpg"),
+    alt: "man on antique phone"
   },
   {
     id: 4,
     year: 2019,
-    imageSrc: require("../assets/images/top-hat.jpg")
+    imageSrc: require("../assets/images/top-hat.jpg"),
+    alt: "man with top hat"
   },
   {
     id: 5,
     year: 2018,
-    imageSrc: require("../assets/images/hiker.jpg")
+    imageSrc: require("../assets/images/hiker.jpg"),
+    alt: "hiker"
   },
   {
     id: 6,
     year: 2022,
-    imageSrc: require("../assets/images/flower-crown.jpg")
+    imageSrc: require("../assets/images/flower-crown.jpg"),
+    alt: "girl with flower crown"
   },
   {
     id: 7,
     year: 1999,
-    imageSrc: require("../assets/images/crossing-arms.jpg")
+    imageSrc: require("../assets/images/crossing-arms.jpg"),
+    alt: "man crossing arms"
   }
 ];
