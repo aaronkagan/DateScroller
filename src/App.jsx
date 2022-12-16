59d685a048eedbc48061fc138e8cf4157a0ee713
Aaron Kagan <aaronkagandev@gmail.com>
2022-12-15 22:02:33 -0500

    adding mock image data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import GlobalStyle from "./style/GlobalStyle";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div>
+    <Wrapper>
       <GlobalStyle />
-      <h1>Hello</h1>
-      <button>Test</button>
-    </div>
+      <Footer>
+        All images by <a href="https://unsplash.com/@dtolokonov?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Dmytro Tolokonov</a>
+      </Footer>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div``;
+
+const Footer = styled.div``;
 
 export default App;
---
+++ src/data/imageData.js
@@ -0,0 +1,37 @@
+export const imageData = [
+  {
+    id: 1,
+    year: 2016,
+    imageSrc: require("../assets/images/leaves.jpg")
+  },
+  {
+    id: 2,
+    year: 2020,
+    imageSrc: require("../assets/images/bw-portrait.jpg")
+  },
+  {
+    id: 3,
+    year: 2015,
+    imageSrc: require("../assets/images/man-on-phone.jpg")
+  },
+  {
+    id: 4,
+    year: 2019,
+    imageSrc: require("../assets/images/top-hat.jpg")
+  },
+  {
+    id: 5,
+    year: 2018,
+    imageSrc: require("../assets/images/hiker.jpg")
+  },
+  {
+    id: 6,
+    year: 2022,
+    imageSrc: require("../assets/images/flower-crown.jpg")
+  },
+  {
+    id: 7,
+    year: 1999,
+    imageSrc: require("../assets/images/crossing-arms.jpg")
+  }
+];
